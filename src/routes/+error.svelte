e34854691b31e2e2e9a5178e5bc3b1c34179981a
scosman <scosman@users.noreply.github.com>
2023-08-22 00:39:45 -0400

    Now we're going to bring our tools together, and add some basic content. Using componets from DaisyUI we create basic scaffolding for the webpage. Most importantly a layout which is re-used across all pages.
     - Modify +layout.svelte to add a footer below the page content
     - Modify +layout.svelte to add a header above the page content, including a responsive menu for mobile
     - Modify +page.svelte to add some simple homepage content
     - Add +error.svelte for 404s are other errors
    
    Note: some links are 404s. That's okay, we'll clean this up in later commits

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<div class="hero min-h-[60vh] bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-lg">
+        <h1 class="text-5xl font-bold">This is embarassing...</h1>
+        <p class="py-6 text-2xl">There was an error: {$page.error.message}</p>
+      </div>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero min-h-[60vh] bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Sass Starter</h1>
+        <p class="py-6 text-2xl">A fast, free to host, open source Saas marketing page template.</p>
+        <a href="https://github.com/CriticalMoments/CMSassStarter">
+            <button class="btn btn-primary">Get Started</button>
+        </a>
+      </div>
+    </div>
+</div>
