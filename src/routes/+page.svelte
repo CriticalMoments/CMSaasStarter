50a8f18ff6343a061e60ffbad0e298e85782b8a8
scosman <scosman@users.noreply.github.com>
2023-08-21 22:51:51 -0400

    Install tailwindcss and intergrate with SvelteKit.
    Followed offical docs here: https://tailwindcss.com/docs/guides/sveltekit
    
    Permlink: https://web.archive.org/web/20230804095936/https://tailwindcss.com/docs/guides/sveltekit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +8,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
