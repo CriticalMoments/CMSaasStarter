dd4c5b34f02fe589ab00db42654c0a6b5511f801
scosman <scosman@users.noreply.github.com>
2023-08-24 00:15:39 -0400

    I am sassy. But this is SaaSy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <div class="hero min-h-[60vh] bg-base-200">
     <div class="hero-content text-center">
       <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Sass Starter</h1>
+        <h1 class="text-5xl font-bold">SaaS Starter</h1>
         <p class="py-6 text-2xl">A fast, free to host, open source Saas marketing page template.</p>
-        <a href="https://github.com/CriticalMoments/CMSassStarter">
+        <a href="https://github.com/CriticalMoments/CMSaasStarter">
             <button class="btn btn-primary">Get Started</button>
         </a>
       </div>
