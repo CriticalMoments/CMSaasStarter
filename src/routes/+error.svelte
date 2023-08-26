4ba7c91ee87ff5101733088f92176ce5a3943c04
scosman <scosman@users.noreply.github.com>
2023-08-25 21:53:10 -0400

    Fix bug on error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,9 +1,9 @@
 <script>
-    import "../../app.css";
+    import "../app.css";
     import { page } from '$app/stores';
 </script>
 
-<div class="hero min-h-[60vh] bg-base-200">
+<div class="hero min-h-[100vh] bg-base-200">
     <div class="hero-content text-center">
       <div class="max-w-lg">
         <h1 class="text-5xl font-bold">This is embarassing...</h1>
