f523ffad9282e4f5a7f8978eec792cca4cf37c3f
scosman <scosman@users.noreply.github.com>
2023-09-08 16:10:39 -0400

    "Marketing" page content

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -8,7 +8,7 @@
     </div>
     <div class="flex-none">
         <ul class="menu menu-horizontal px-1 hidden sm:flex">
-                <li><a href="https://github.com/CriticalMoments/CMSaasStarter">Docs</a></li>
+                <li><a href="https://github.com/CriticalMoments/CMSaasStarter" class="bg-base-200">★ us on Github</a></li>
                 <li><a href="/blog">Blog</a></li>
                 <li><a href="/pricing">Pricing</a></li>
                 <li><a href="/account">Account</a></li>
@@ -18,10 +18,10 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
             </label>
             <ul tabindex="0" class="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a href="https://github.com/CriticalMoments/CMSaasStarter">Docs</a></li>
                 <li><a href="/blog">Blog</a></li>
                 <li><a href="/pricing">Pricing</a></li>
                 <li><a href="/account">Account</a></li>
+                <li><a href="https://github.com/CriticalMoments/CMSaasStarter" class="bg-base-200">★ us on Github</a></li>
             </ul>
         </div>
     </div>
--- src/routes/(marketing)/+page.svelte
+++ src/routes/(marketing)/+page.svelte
@@ -1,11 +1,74 @@
 <div class="hero min-h-[60vh] bg-base-200">
-    <div class="hero-content text-center">
+    <div class="hero-content text-center py-12">
       <div class="max-w-md">
-        <h1 class="text-5xl font-bold">SaaS Starter</h1>
-        <p class="py-6 text-2xl">A fast, free to host, open source Saas marketing page template.</p>
+        <div class="text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary to-pink-600">
+          SaaS Starter Demo
+        </div>
+        <div class="my-6 text-xl">
+          SaaS Starter is a SaaS marketing page template which is open source, fast and free to host.
+        </div>
+        <div class="my-6 text-xl">
+          Built with <a href="https://kit.svelte.dev" class="link" target="_blank">SvelteKit</a>, <a href="https://supabase.com" class="link" target="_blank">Supabase</a>, and <a href="https://stripe.com" class="link" target="_blank">Stripe</a>
+        </div>
         <a href="https://github.com/CriticalMoments/CMSaasStarter">
-            <button class="btn btn-primary">Get Started</button>
+            <button class="btn btn-primary btn-wide">★ us on Github</button>
         </a>
+        <a href="https://github.com/CriticalMoments/CMSaasStarter/blob/main/README.md">
+            <button class="btn btn-outline btn-primary btn-wide mt-3">Read the Docs</button>
+        </a>
+      </div>
+    </div>
+</div>
+<div class="hero min-h-[60vh]">
+    <div class="hero-content text-center py-20">
+      <div class="max-w-md">
+        <div class="text-5xl font-bold">Explore the Demo</div>
+        <div class="mt-6 text-xl">
+          This is a fully functional demo page. Try it out!
+        </div>
+        
+        <div class="mt-5 text-l text-left max-w-[288px] mx-8 mx-auto">
+          <ul class="list-disc">
+            <li class="">A customizeable <a href="/pricing" class="link">pricing page</a></li>
+            <li class="mt-2">A <a href="/blog" class="link">blog</a> with <a href="http://localhost:5173/blog/example_blog_post" class="link">rich formatting</a>, and <a href="/blog/rss.xml" class="link">RSS</a></li>
+            <li class="mt-2">
+              Authentication with <a href="/login/sign_up" class="link">sign up</a>, <a href="/login/sign_in" class="link">sign in</a>, and <a href="/login/forgot_password" class="link">recover password</a>
+            </li>
+            <li class="mt-2">
+              Stripe Checkout integration for <a href="/pricing" class=link>purchasing subscriptions</a>. This demo page allows test credit card numbers. 
+            </li>
+            <li class="mt-2">
+              Stripe Portal integration for <a href="/account/billing" class="link">updating payments, invoices, canceling and upgrading.</a>
+            </li>
+            <li class="mt-2">
+              User manageable settings for <a href="/account/settings" class="link">updating profile, changing email and changing password.</a>
+            </li>
+            <li class="mt-2">
+              Perfect 100/100 <a href="https://pagespeed.web.dev" class="link" target="_blank">Google performance scores</a>
+            </li>
+            <li class="mt-2">
+              Fully responsive for mobile or desktop
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+</div>
+<div class="hero min-h-[60vh] bg-base-200">
+    <div class="hero-content text-center py-12 px-4">
+      <div class="max-w-md">
+        <div class="text-5xl font-bold mt-8">See it in Action</div>
+        <div class="mt-6 text-xl">
+          SaaS Started was created and sponsored by <a href="https://criticalmoments.io" class="link" target="_blank">Critical Moments</a> 
+        </div>
+        <div class="mt-6 text-xl">
+          Our <a href="https://criticalmoments.io" class="link" target="_blank">webpage</a> is the best example of SaaS Starter with style and real content.
+        </div>
+        <div class="mt-6 text-large">
+        <a href="https://criticalmoments.io" target="_blank">
+            <button class="btn btn-primary btn-wide mt-3">See it in Action</button>
+        </a>
+        </div>
       </div>
     </div>
 </div>
