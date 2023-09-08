9a6a854e1d9a5c3f8df0f9f9159aea51ac661832
scosman <scosman@users.noreply.github.com>
2023-09-08 12:20:59 -0400

    Improve mobile usability w bigger links

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -17,7 +17,7 @@
             <label tabindex="0" class="btn btn-ghost btn-circle">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
             </label>
-            <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <ul tabindex="0" class="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                 <li><a href="https://github.com/CriticalMoments/CMSaasStarter">Docs</a></li>
                 <li><a href="/blog">Blog</a></li>
                 <li><a href="/pricing">Pricing</a></li>
