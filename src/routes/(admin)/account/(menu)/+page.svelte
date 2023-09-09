eff0b3ffc4fa731ae727b07ee3b32cc89e5e10d5
scosman <scosman@users.noreply.github.com>
2023-09-09 17:58:04 -0400

    design tweaks

--- src/routes/(admin)/account/(menu)/+page.svelte
+++ src/routes/(admin)/account/(menu)/+page.svelte
@@ -4,12 +4,10 @@
 
 	let adminSection: Writable<String> = getContext('adminSection');
 	adminSection.set('home')
-
-
 </script>
 
 <h1 class="text-2xl font-bold mb-1">Dashboard</h1>
-<div class="alert alert-error">
+<div class="alert alert-error max-w-lg mt-2">
 	<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
 	<div>
 		<div class="font-bold">Demo Content</div>
