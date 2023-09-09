e379ea9a6fe26d3d4e1682281788a66d9e8bda86
scosman <scosman@users.noreply.github.com>
2023-09-09 09:38:52 -0400

    Better dashbaord warning

--- src/routes/(admin)/account/(menu)/+page.svelte
+++ src/routes/(admin)/account/(menu)/+page.svelte
@@ -9,14 +9,18 @@
 </script>
 
 <h1 class="text-2xl font-bold mb-1">Dashboard</h1>
-<div class="badge badge-warning gap-2">Non Functional Demo Content</div>
-<p class="text-base mt-4 max-w-[420px]">
-	This page is just a placeholder. 
-	Replace this page with your app's content and functionality.
-</p>
-<p class="text-base mb-8 mt-3 max-w-[420px]">
-	The billing and settings pages are functional demos. 
-</p>
+<div class="alert alert-error">
+	<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+	<div>
+		<div class="font-bold">Demo Content</div>
+		<div class="my-2">
+			This page is just a placeholder. Replace this page with your app's content and functionality.
+		</div>
+		<div class="my-2">
+			The <a href="/account/billing" class="link">billing</a> and <a href="/account/settings" class="link">settings</a> pages are functional demos. 
+		</div>
+	</div>
+  </div>
 
 <div class="my-6">
 	<h1 class="text-xl font-bold mb-1">Users</h1>
