d5e88ba568e07635a8d3665a378e0e8c701778f1
scosman <scosman@users.noreply.github.com>
2023-09-11 12:30:08 -0400

    Close drawer on mobile when you pick an item.

--- src/routes/(admin)/account/(menu)/+layout.svelte
+++ src/routes/(admin)/account/(menu)/+layout.svelte
@@ -9,6 +9,10 @@
 	adminSectionStore.subscribe((value) => {
 		adminSection = value
 	})
+
+	function closeDrawer():void {
+		document.getElementById('admin-drawer').checked = false
+	}
 </script>
 
 <div class="drawer lg:drawer-open">
@@ -43,14 +47,13 @@
 			</div>
 		</li>
 		<li>
-			<a href="/account" class="{adminSection === 'home' ? 'active' : ''}">
+			<a href="/account" class="{adminSection === 'home' ? 'active' : ''}" on:click={closeDrawer}>
 				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
-
 				Home
 			</a>
 		</li>
 		<li>
-			<a href='/account/billing' class="{adminSection === 'billing' ? 'active' : ''}">
+			<a href='/account/billing' class="{adminSection === 'billing' ? 'active' : ''}" on:click={closeDrawer}>
 				<svg class="h-5 w-5" viewBox="0 0 24 24" stroke="none" fill="currentColor">
 					<path d="M18,1H6A3,3,0,0,0,3,4V22a1,1,0,0,0,1.8.6L6.829,19.9l1.276,2.552a1,1,0,0,0,.8.549.981.981,0,0,0,.89-.4L12,19.667,14.2,22.6a.983.983,0,0,0,.89.4,1,1,0,0,0,.8-.549L17.171,19.9,19.2,22.6a1,1,0,0,0,.8.4,1,1,0,0,0,1-1V4A3,3,0,0,0,18,1Zm1,18-1.2-1.6a.983.983,0,0,0-.89-.4,1,1,0,0,0-.8.549l-1.276,2.552L12.8,17.4a1,1,0,0,0-1.6,0L9.171,20.105,7.9,17.553A1,1,0,0,0,7.09,17a.987.987,0,0,0-.89.4L5,19V4A1,1,0,0,1,6,3H18a1,1,0,0,1,1,1ZM17,9a1,1,0,0,1-1,1H8A1,1,0,0,1,8,8h8A1,1,0,0,1,17,9Zm-4,4a1,1,0,0,1-1,1H8a1,1,0,0,1,0-2h4A1,1,0,0,1,13,13Z"/>
 				</svg>
@@ -58,7 +61,7 @@
 			</a>
 		</li>
 		<li>
-			<a href= "/account/settings" class="{adminSection === 'settings' ? 'active' : ''}">
+			<a href= "/account/settings" class="{adminSection === 'settings' ? 'active' : ''}" on:click={closeDrawer}>
 				<svg class="h-5 w-5" viewBox="0 0 24 24" stroke="none" fill="none">
 					<g id="Interface / Settings">
 						<g id="Vector">
