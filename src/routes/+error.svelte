77f8f9f9e621e9c9d627e1ca1f8eeeb3b535289b
scosman <scosman@users.noreply.github.com>
2023-09-13 09:17:53 -0400

    Add home button on error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,6 +8,9 @@
     <div class="max-w-lg">
       <h1 class="text-5xl font-bold">This is embarassing...</h1>
       <p class="py-6 text-2xl">There was an error: {$page?.error?.message}</p>
+      <div>
+        <a href="/" class="btn btn-primary btn-wide">Return Home</a>
+      </div>
     </div>
   </div>
 </div>
