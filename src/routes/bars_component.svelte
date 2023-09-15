a80c02c922cee36cf60c1bc4f28ac3290e9cfd4c
scosman <scosman@users.noreply.github.com>
2023-09-15 15:40:51 -0400

    Fix rendering artifacts in animation

--- src/routes/bars_component.svelte
+++ src/routes/bars_component.svelte
@@ -7,9 +7,15 @@
 </script>
 
 {#each barColors as barColor, i}
-  <div class="relative {barColor}" style="height:{halfBarHeight}vh;"></div>
+  <div class="relative {barColor}" style="height:{halfBarHeight}vh;">
+    <!-- inner bar prevents any artifacts if rendering is off by subpixel amounts -->
+    <div
+      class="{barColor} absolute w-full"
+      style="height:{halfBarHeight * 2 + 0.1}vh;"
+    ></div>
+  </div>
   <div
     class="sticky z-10 {barColor}"
-    style="height:{halfBarHeight + 0.25}vh; top:{i * halfBarHeight}vh;"
+    style="height:{halfBarHeight + 0.05}vh; top:{i * halfBarHeight}vh;"
   ></div>
 {/each}
