b9fed26d2345281fd87df24555133307ed6ffe30
scosman <scosman@users.noreply.github.com>
2023-09-17 08:40:50 -0400

    padding

--- src/routes/(marketing)/pricing/+page.svelte
+++ src/routes/(marketing)/pricing/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import PricingModule from "./pricing_module.svelte"
   import { PUBLIC_SITE_NAME } from "$env/static/public"
-  import BarsComponent from "../../bars_component.svelte"
 </script>
 
 <svelte:head>
@@ -9,7 +8,7 @@
   <meta name="description" content="Pricing details for {PUBLIC_SITE_NAME}" />
 </svelte:head>
 
-<div class="min-h-[70vh] py-8 px-6 flex flex-col place-content-center">
+<div class="min-h-[70vh] py-8 lg:py-12 px-6 flex flex-col place-content-center">
   <h1 class="font-header text-3xl md:text-5xl text-center">Pricing</h1>
   <h2 class="pt-2 text-xl md:text-2xl text-center text-slate-600 mt-1 pb-3">
     Real pricing coming soon!
