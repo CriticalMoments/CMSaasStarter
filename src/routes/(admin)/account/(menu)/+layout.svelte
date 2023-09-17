c5bb3149d367e4b6bd48d58b38c01e9caed8db4b
scosman <scosman@users.noreply.github.com>
2023-09-17 14:13:15 -0400

    padding tweaks

--- src/routes/(admin)/account/(menu)/+layout.svelte
+++ src/routes/(admin)/account/(menu)/+layout.svelte
@@ -40,7 +40,7 @@
         </div>
       </div>
     </div>
-    <div class="container px-6 lg:px-12 py-3 lg:py-6">
+    <div class="container px-6 lg:px-12 pb-10 lg:pt-8">
       <slot />
     </div>
   </div>
