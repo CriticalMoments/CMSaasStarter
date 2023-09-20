5675af8008b6061f1778f58a06a6166d3b57be83
scosman <scosman@users.noreply.github.com>
2023-09-20 13:06:13 -0400

    cute cookie warning

--- src/routes/(marketing)/login/+layout.svelte
+++ src/routes/(marketing)/login/+layout.svelte
@@ -1,7 +1,21 @@
+<script lang="ts">
+  let isEurope = false
+  try {
+    isEurope = Intl.DateTimeFormat()
+      .resolvedOptions()
+      .timeZone.startsWith("Europe/")
+  } catch (e) {
+  } finally {
+  }
+</script>
+
 <div
   class="text-center content-center max-w-lg mx-auto min-h-[75vh] mb-12 flex items-center place-content-center"
 >
   <div class="flex flex-col w-64 lg:w-80">
     <slot />
+    <div class="mt-8 {isEurope ? 'block' : 'hidden'}">
+      👋🇪🇺. Logging in uses 🍪s.
+    </div>
   </div>
 </div>
