e6a8b60408b561e8e5526d087e5181360f3bfbbc
scosman <scosman@users.noreply.github.com>
2024-01-16 17:09:21 -0500

    Improve blog page, including responsive mobile support

--- src/routes/(marketing)/blog/+page.svelte
+++ src/routes/(marketing)/blog/+page.svelte
@@ -14,10 +14,10 @@
 
 <div class="py-8 lg:py-12 px-6 max-w-lg mx-auto">
   <div
-    class="text-3xl lg:text-5xl font-medium text-primary flex gap-3 items-baseline"
+    class="text-3xl lg:text-5xl font-medium text-primary flex gap-3 items-baseline text-center place-content-center"
   >
     <div
-      class="flex-none leading-relaxed font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary to-accent"
+      class="text-center leading-relaxed font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary to-accent"
     >
       {blogInfo.name}
     </div>
@@ -29,13 +29,13 @@
       />
     </a>
   </div>
-  <div class="text-lg">A demo blog with sample content.</div>
+  <div class="text-lg text-center">A demo blog with sample content.</div>
 
   {#each sortedPosts as post}
     <a href={post.link}>
       <div class="card my-6 bg-white shadow-xl flex-row overflow-hidden">
-        <div class="flex-none w-32 bg-secondary"></div>
-        <div class="py-6 px-4">
+        <div class="flex-none w-6 md:w-32 bg-secondary"></div>
+        <div class="py-6 px-6">
           <div class="text-xl">{post.title}</div>
           <div class="text-sm text-accent">
             {post.parsedDate.toLocaleDateString("en-US", {
