15771b19de906c74e2df90cda0149ccd6f50a66c
scosman <scosman@users.noreply.github.com>
2024-03-11 14:26:55 -0400

    Add a blog post entry linking to the big blog post explaining how/why we built this template. Includes useful best practices for how to customize this template.

--- src/routes/(marketing)/blog/posts.ts
+++ src/routes/(marketing)/blog/posts.ts
@@ -15,6 +15,12 @@ export type BlogPost = {
 // Create a page in the "(posts)" directory for each entry
 const blogPosts: BlogPost[] = [
   {
+    title: "How we built a beautiful 41kb SaaS website with this template",
+    description: "How to use this template you to bootstrap your own site.",
+    link: "/blog/how_we_built_our_saas_website",
+    date: "2024-03-10",
+  },
+  {
     title: "Example Blog Post 2",
     description: "Even more example content!",
     link: "/blog/awesome_post",
