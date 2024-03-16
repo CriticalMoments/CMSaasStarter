60d0b3b84c5f26267ca5fb4d503619c79677155f
Charl Best <charl.best@toasttab.com>
2024-03-16 14:51:52 +0000

    Basic language support (i18n)

---
+++ src/lib/translate.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { t } from "$lib/i18n"
+  export let k: string
+  export let v = {}
+</script>
+
+<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+{@html $t(k, v)}
