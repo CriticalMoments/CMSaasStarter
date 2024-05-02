c27bd67f2985a479cab5d41a8294379bc603c74a
Wasyl Mowczan <wasyl.mowczan@tsh.io>
2024-05-02 16:31:40 +0200

    Add api to generate new page

--- src/routes/(admin)/account/(menu)/+page.server.ts
+++ src/routes/(admin)/account/(menu)/+page.server.ts
@@ -1,4 +1,5 @@
 import { redirect } from "@sveltejs/kit"
+import { STABILITY_API_KEY } from "$env/static/private"
 
 export const actions = {
   signout: async ({ locals: { supabase, getSession } }) => {
@@ -8,4 +9,80 @@ export const actions = {
       throw redirect(303, "/")
     }
   },
+  new: async ({ request, locals: { supabase, getSession } }) => {
+    const session = await getSession()
+    if (!session) {
+      throw redirect(303, "/login")
+    }
+
+    const profile = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", session?.user?.id?.toString())
+      .limit(1)
+
+    // call the Stability AI endpoint and generate the imag
+    const formData = await request.formData()
+    const prompt = formData.get("prompt")
+
+    const engineId = "stable-diffusion-xl-1024-v1-0"
+    const apiHost = "https://api.stability.ai"
+    const apiKey = STABILITY_API_KEY
+
+    const res = await fetch(
+      `${apiHost}/v1/generation/${engineId}/text-to-image`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "image/png",
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+          text_prompts: [
+            {
+              text:
+                prompt +
+                ", line art outline, friendly, can be used as a coloring sheet, minimalistic, black and white, simple, few details, thin lines, lots of white space",
+              weight: 1,
+            },
+          ],
+          disable_safety_checker: true,
+          negative_prompt:
+            "color, ugly, low-res, deformed, mutation, blurry, malformed, disgusting, mutilated, mangled, old, color, dark, black",
+          cfg_scale: 10,
+          clip_guidance_preset: "FAST_BLUE",
+          sampler: "DDIM",
+          height: 1216,
+          width: 832,
+          steps: 50,
+          samples: 1,
+        }),
+      },
+    )
+    const imageBlob = await res.blob()
+
+    // save the blob to supabase storage
+    const imageId = crypto.randomUUID()
+    const imagePath = `${profile.data?.[0]?.id}/${imageId}`
+    const file = new File([imageBlob], imageId, { type: "image/png" })
+    await supabase.storage.from("pages").upload(imagePath, file)
+
+    // add information to images table
+    try {
+      await supabase
+        .from("images")
+        .insert([
+          {
+            owner_id: profile.data?.[0]?.id,
+            prompt: prompt,
+          },
+        ])
+        .select()
+    } catch (error) {
+      return { error: error }
+    }
+  },
+
+  // get all images of the user
 }
--- src/routes/(admin)/account/(menu)/+page.svelte
+++ src/routes/(admin)/account/(menu)/+page.svelte
@@ -1,13 +1,70 @@
 <script lang="ts">
-  import { getContext } from "svelte"
-  import type { Writable } from "svelte/store"
+  import { getContext, onMount } from "svelte"
+  import { writable, type Writable } from "svelte/store"
+  import { enhance } from "$app/forms"
+  export let data
 
+  let isLoading = false
   let adminSection: Writable<string> = getContext("adminSection")
   adminSection.set("home")
+
+  // Fetch images from server endpoint
+  async function fetchImages() {
+    const response = await fetch("/api/getImages")
+    const { images } = await response.json()
+    return images
+  }
+
+  // Store to hold image URLs
+  const images = writable([])
+  onMount(async () => {
+    const fetchedImages = await fetchImages()
+    images.set(fetchedImages)
+  })
 </script>
 
 <svelte:head>
   <title>Account</title>
 </svelte:head>
 
-<h1 class="text-2xl font-bold mb-1">Dashboard</h1>
+<h1 class="text-2xl font-bold mb-1">Create new coloring page</h1>
+
+<div class="grow w-full flex items-center justify-cente">
+  <form
+    class="bg-neutral-100 shadow-md p-8"
+    method="POST"
+    action="?/new"
+    use:enhance={() => {
+      isLoading = true
+    }}
+  >
+    <label for="prompt" class="block font-extralight tracking-wide px-2 py-1"
+      >Enter Your Prompt Here:</label
+    >
+    <textarea name="prompt" class="w-[500px] h-[100px] px-2 py-1" />
+    <div class="w-full flex justify-center mt-4">
+      {#if !isLoading}
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="submit">Generate</button
+        >
+      {:else}
+        <button
+          class="bg-neutral-400 text-white font-bold py-2 px-4 rounded"
+          disabled={true}>Loading...</button
+        >
+      {/if}
+    </div>
+  </form>
+</div>
+
+<hr class="border-t-2 border-gray-300 my-6" />
+<h2 class="font-bold text-2xl">Coloring Pages</h2>
+<p class="text-slate-500">
+  View all your previously created images here. Click on any image to download
+  it.
+</p>
+
+<div>
+  <div></div>
+</div>
