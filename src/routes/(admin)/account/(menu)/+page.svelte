85226c0ae9bdffc70c040d170a4cbed54661559e
Wasyl Mowczan <wasyl.mowczan@tsh.io>
2024-05-20 11:39:16 +0200

    Update text on Generate button

--- src/routes/(admin)/account/(menu)/+page.svelte
+++ src/routes/(admin)/account/(menu)/+page.svelte
@@ -98,7 +98,7 @@
         {:else}
           <button
             class="bg-neutral-400 text-white font-bold py-2 px-4 rounded"
-            disabled={isLoading}>Loading...</button
+            disabled={isLoading}>Loading... (~10-15 seconds)</button
           >
         {/if}
       </div>
