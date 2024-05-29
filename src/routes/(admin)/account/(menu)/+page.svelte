f3136c3c09421b19230ef45cc7301403053d31f0
leo mclaughlin <leo.mcl616@gmail.com>
2024-05-29 17:26:02 +0100

    renders compoenst based on plan

--- src/routes/(admin)/account/(menu)/+page.svelte
+++ src/routes/(admin)/account/(menu)/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { getContext } from "svelte";
   import type { Writable } from "svelte/store";
-  import PricingModule from "../../../(marketing)/pricing/pricing_module.svelte";
   import { pricingPlans, defaultPlanId } from "../../../(marketing)/pricing/pricing_plans";
 
   let adminSection: Writable<string> = getContext("adminSection");
@@ -14,7 +13,6 @@
     (x) => x.id === data.currentPlanId
   )?.name;
   
-  console.log(currentPlanId, currentPlanName);
 </script>
 
 <svelte:head>
@@ -22,7 +20,8 @@
 </svelte:head>
 
 <h1 class="text-2xl font-bold mb-1">Dashboard</h1>
-<div class="alert alert-error max-w-lg mt-2">
+
+<!-- <div class="alert alert-error max-w-lg mt-2">
   <svg
     xmlns="http://www.w3.org/2000/svg"
     class="stroke-current shrink-0 h-6 w-6"
@@ -47,53 +46,55 @@
       demos.
     </div>
   </div>
-</div>
+</div> -->
 
-<div class="my-6">
-  <h1 class="text-xl font-bold mb-1">Users</h1>
-  <div class="stats shadow stats-vertical sm:stats-horizontal sm:w-[420px]">
-    <div class="stat place-items-center">
-      <div class="stat-title">Downloads</div>
-      <div class="stat-value">31K</div>
-      <div class="stat-desc">↗︎ 546 (2%)</div>
-    </div>
+{#if currentPlanName === "Pro"}
+  <div class="my-6">
+    <h1 class="text-xl font-bold mb-1">Users</h1>
+    <div class="stats shadow stats-vertical sm:stats-horizontal sm:w-[420px]">
+      <div class="stat place-items-center">
+        <div class="stat-title">Downloads</div>
+        <div class="stat-value">31K</div>
+        <div class="stat-desc">↗︎ 546 (2%)</div>
+      </div>
 
-    <div class="stat place-items-center">
-      <div class="stat-title">Users</div>
-      <div class="stat-value text-secondary">4,200</div>
-      <div class="stat-desc">↗︎ 40 (2%)</div>
+      <div class="stat place-items-center">
+        <div class="stat-title">Users</div>
+        <div class="stat-value text-secondary">4,200</div>
+        <div class="stat-desc">↗︎ 40 (2%)</div>
+      </div>
     </div>
   </div>
-</div>
-<div class="my-6">
-  <h1 class="text-xl font-bold mb-1">Accounts</h1>
-  <div class="stats shadow stats-vertical sm:stats-horizontal sm:w-[420px]">
-    <div class="stat place-items-center">
-      <div class="stat-title">New Registers</div>
-      <div class="stat-value">1,200</div>
-      <div class="stat-desc">↘︎ 90 (14%)</div>
-    </div>
+  <div class="my-6">
+    <h1 class="text-xl font-bold mb-1">Accounts</h1>
+    <div class="stats shadow stats-vertical sm:stats-horizontal sm:w-[420px]">
+      <div class="stat place-items-center">
+        <div class="stat-title">New Registers</div>
+        <div class="stat-value">1,200</div>
+        <div class="stat-desc">↘︎ 90 (14%)</div>
+      </div>
 
-    <div class="stat place-items-center">
-      <div class="stat-title">Churned Accounts</div>
-      <div class="stat-value">42</div>
-      <div class="stat-desc">↘︎ 6 (12%)</div>
+      <div class="stat place-items-center">
+        <div class="stat-title">Churned Accounts</div>
+        <div class="stat-value">42</div>
+        <div class="stat-desc">↘︎ 6 (12%)</div>
+      </div>
     </div>
   </div>
-</div>
-<div class="my-6">
-  <h1 class="text-xl font-bold mb-1">Revenue</h1>
-  <div class="stats shadow stats-vertical sm:stats-horizontal sm:w-[420px]">
-    <div class="stat place-items-center">
-      <div class="stat-title text-success">Revenue</div>
-      <div class="stat-value text-success">$4200</div>
-      <div class="stat-desc">↗︎ $180 (4%)</div>
-    </div>
+  <div class="my-6">
+    <h1 class="text-xl font-bold mb-1">Revenue</h1>
+    <div class="stats shadow stats-vertical sm:stats-horizontal sm:w-[420px]">
+      <div class="stat place-items-center">
+        <div class="stat-title text-success">Revenue</div>
+        <div class="stat-value text-success">$4200</div>
+        <div class="stat-desc">↗︎ $180 (4%)</div>
+      </div>
 
-    <div class="stat place-items-center">
-      <div class="stat-title">New Subscribers</div>
-      <div class="stat-value">16</div>
-      <div class="stat-desc">↘︎ 1 (%7)</div>
+      <div class="stat place-items-center">
+        <div class="stat-title">New Subscribers</div>
+        <div class="stat-value">16</div>
+        <div class="stat-desc">↘︎ 1 (%7)</div>
+      </div>
     </div>
   </div>
-</div>
+{/if}
