c2972e78616b41060e2d523e6138e1050a7ea855
scosman <scosman@users.noreply.github.com>
2024-07-29 20:22:37 -0400

    Swap fuse for lunr. Lunr has real inverted search index, and better word matching (price -> pricing).

--- src/lib/build_index.ts
+++ src/lib/build_index.ts
@@ -3,19 +3,25 @@ import fs from "fs"
 import glob from "glob"
 import { convert } from "html-to-text"
 import JSDOM from "jsdom"
-import Fuse from "fuse.js"
+import lunr from "lunr"
 
 const excludePaths = ["/search"]
 
 export async function buildSearchIndex() {
-  const indexData = []
+  const docs = []
+  const indexDocs: {
+    title: string
+    description: string
+    body: string
+    id: number
+  }[] = []
 
   // iterate all files with html extension in ./svelte-kit/output/prerendered/pages
   const fileRoot = path.resolve(".")
   const pagesPath = path.join(fileRoot, ".svelte-kit/output/prerendered/pages")
 
   const allFiles = glob.sync(path.join(pagesPath, "**/*.html"))
-  for (const file of allFiles) {
+  for (const [i, file] of allFiles.entries()) {
     try {
       const webPath = file
         .replace(pagesPath, "")
@@ -43,23 +49,42 @@ export async function buildSearchIndex() {
         dom.window.document
           .querySelector('meta[name="description"]')
           ?.getAttribute("content") || ""
-      indexData.push({
+      docs.push({
+        title,
+        description,
+        path: webPath,
+      })
+      indexDocs.push({
         title,
         description,
         body: plaintext,
-        path: webPath,
+        id: i,
       })
     } catch (e) {
       console.log("Blog search indexing error", file, e)
     }
   }
 
-  const index = Fuse.createIndex(["title", "description", "body"], indexData)
-  const jsonIndex = index.toJSON()
-  const data = { index: jsonIndex, indexData, buildTime: Date.now() }
-  return data
+  const index = lunr(function () {
+    this.field("title", { boost: 3 })
+    this.field("description", { boost: 2 })
+    this.field("body", { boost: 1 })
+    this.ref("id")
+
+    indexDocs.forEach((doc) => {
+      this.add(doc)
+    }, this)
+  })
+
+  return {
+    index: JSON.stringify(index),
+    docs,
+    buildTime: Date.now(),
+  }
 }
 
+// Use this if you want to integrate intyou your build process manually.
+// Default install achieves similar result by setting prerender=true fore /search/api route.
 export async function buildAndCacheSearchIndex() {
   const data = await buildSearchIndex()
   // write index data to file, overwriting static file on build
