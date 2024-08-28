926a24ae11a85b2fbfcea9cdc2ceb4ee391c7536
Louis Deconinck <louis.dck@gmail.com>
2024-08-28 15:25:56 +0200

    Dynamic menu & footer

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -1,6 +1,33 @@
 <script>
   import { WebsiteName } from "./../../config"
   import "../../app.css"
+
+  const menuItems = [
+    {
+      label: "★ us on Github",
+      url: "https://github.com/CriticalMoments/CMSaasStarter",
+      special: true,
+    },
+    { label: "Blog", url: "/blog" },
+    { label: "Pricing", url: "/pricing" },
+    { label: "Account", url: "/account" },
+    {
+      label: "Search",
+      url: "/search",
+      icon: "M17.545 15.467l-3.779-3.779a6.15 6.15 0 0 0 .898-3.21c0-3.417-2.961-6.377-6.378-6.377A6.185 6.185 0 0 0 2.1 8.287c0 3.416 2.961 6.377 6.377 6.377a6.15 6.15 0 0 0 3.115-.844l3.799 3.801a.953.953 0 0 0 1.346 0l.943-.943c.371-.371.236-.84-.135-1.211zM4.004 8.287a4.282 4.282 0 0 1 4.282-4.283c2.366 0 4.474 2.107 4.474 4.474a4.284 4.284 0 0 1-4.283 4.283c-2.366-.001-4.473-2.109-4.473-4.474z",
+    },
+  ]
+
+  const footerLinks = [
+    { label: "Overview", url: "/" },
+    { label: "Pricing", url: "/pricing" },
+    { label: "Blog", url: "/blog" },
+    { label: "Contact Us", url: "/contact_us" },
+    {
+      label: "Github",
+      url: "https://github.com/CriticalMoments/CMSaasStarter",
+    },
+  ]
 </script>
 
 <div class="navbar bg-base-100 container mx-auto">
@@ -9,29 +36,27 @@
   </div>
   <div class="flex-none">
     <ul class="menu menu-horizontal px-1 hidden sm:flex font-bold text-lg">
-      <li class="md:mx-4">
-        <a
-          href="https://github.com/CriticalMoments/CMSaasStarter"
-          class="border border-primary">★ us on Github</a
-        >
-      </li>
-      <li class="md:mx-2"><a href="/blog">Blog</a></li>
-      <li class="md:mx-2"><a href="/pricing">Pricing</a></li>
-      <li class="md:mx-2"><a href="/account">Account</a></li>
-      <li class="md:mx-0">
-        <a href="/search">
-          <svg
-            fill="#000000"
-            class="w-6 h-6"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-            ><path
-              d="M17.545 15.467l-3.779-3.779a6.15 6.15 0 0 0 .898-3.21c0-3.417-2.961-6.377-6.378-6.377A6.185 6.185 0 0 0 2.1 8.287c0 3.416 2.961 6.377 6.377 6.377a6.15 6.15 0 0 0 3.115-.844l3.799 3.801a.953.953 0 0 0 1.346 0l.943-.943c.371-.371.236-.84-.135-1.211zM4.004 8.287a4.282 4.282 0 0 1 4.282-4.283c2.366 0 4.474 2.107 4.474 4.474a4.284 4.284 0 0 1-4.283 4.283c-2.366-.001-4.473-2.109-4.473-4.474z"
-              fill="currentColor"
-            /></svg
+      {#each menuItems as item}
+        <li class="md:mx-2">
+          <a
+            href={item.url}
+            class={item.special ? "border border-primary" : ""}
           >
-        </a>
-      </li>
+            {#if item.icon}
+              <svg
+                fill="#000000"
+                class="w-6 h-6"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d={item.icon} fill="currentColor" />
+              </svg>
+            {:else}
+              {item.label}
+            {/if}
+          </a>
+        </li>
+      {/each}
     </ul>
     <div class="dropdown dropdown-end sm:hidden">
       <!-- svelte-ignore a11y-label-has-associated-control -->
@@ -43,29 +68,41 @@
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          ><path
+        >
+          <path
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
             d="M4 6h16M4 12h16M4 18h7"
-          /></svg
-        >
+          />
+        </svg>
       </label>
       <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
       <ul
         tabindex="0"
         class="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-bold"
       >
-        <li><a href="/blog">Blog</a></li>
-        <li><a href="/pricing">Pricing</a></li>
-        <li><a href="/account">Account</a></li>
-        <li><a href="/search">Search</a></li>
-        <li>
-          <a
-            href="https://github.com/CriticalMoments/CMSaasStarter"
-            class="border border-primary">★ us on Github</a
-          >
-        </li>
+        {#each menuItems as item}
+          <li>
+            <a
+              href={item.url}
+              class={item.special ? "border border-primary" : ""}
+            >
+              {#if item.icon}
+                <svg
+                  fill="#000000"
+                  class="w-6 h-6"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d={item.icon} fill="currentColor" />
+                </svg>
+              {:else}
+                {item.label}
+              {/if}
+            </a>
+          </li>
+        {/each}
       </ul>
     </div>
   </div>
@@ -84,14 +121,9 @@
   >
     <nav>
       <span class="footer-title opacity-80">Explore</span>
-      <a class="link link-hover mb-1" href="/">Overview</a>
-      <a class="link link-hover my-1" href="/pricing">Pricing</a>
-      <a class="link link-hover my-1" href="/blog">Blog</a>
-      <a class="link link-hover my-1" href="/contact_us">Contact Us</a>
-      <a
-        class="link link-hover my-1"
-        href="https://github.com/CriticalMoments/CMSaasStarter">Github</a
-      >
+      {#each footerLinks as link}
+        <a class="link link-hover my-1" href={link.url}>{link.label}</a>
+      {/each}
     </nav>
     <aside>
       <span class="footer-title opacity-80">Sponsor</span>
