3d0cf7930491feb39209c670dcacabba3e3aab77
scosman <scosman@users.noreply.github.com>
2024-08-30 21:11:59 -0400

    Don't recall safeGetSession, already in the hook

--- src/routes/(marketing)/login/+layout.server.ts
+++ src/routes/(marketing)/login/+layout.server.ts
@@ -2,12 +2,11 @@ import { redirect } from "@sveltejs/kit"
 import type { LayoutServerLoad } from "./$types"
 
 export const load: LayoutServerLoad = async ({
-  url,
-  locals: { safeGetSession },
+  locals: { session },
   cookies,
+  url,
 }) => {
   // if the user is already logged in return them to the account page
-  const { session } = await safeGetSession()
   if (session) {
     redirect(303, "/account")
   }
