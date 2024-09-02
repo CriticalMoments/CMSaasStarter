6b60de657badeee56fa4c452b0ec285e1ca023f7
scosman <scosman@users.noreply.github.com>
2024-09-02 11:54:38 -0400

    Fix sign out page
     - Ugly layout
     - If we tried to render server side it errorer (no goto)
     - It was signing out on load, not on mount

--- src/routes/(admin)/account/sign_out/+page.svelte
+++ src/routes/(admin)/account/sign_out/+page.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
   import { goto } from "$app/navigation"
+  import { onMount } from "svelte"
 
   export let data
 
   let { supabase } = data
   let message = "Signing out...."
 
-  supabase.auth.signOut().then(({ error }) => {
-    if (error) {
-      message = "There was an issue signing out."
-    } else {
-      goto("/")
-    }
+  // on mount, sign out
+  onMount(() => {
+    supabase.auth.signOut().then(({ error }) => {
+      if (error) {
+        message = "There was an issue signing out."
+      } else {
+        goto("/")
+      }
+    })
   })
 </script>
 
-<h1 class="text-2xl font-bold m-6">{message}</h1>
+<h1 class="text-2xl font-bold m-6 mx-auto my-auto">{message}</h1>
