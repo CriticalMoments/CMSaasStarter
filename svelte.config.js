cd7a8dbd4bce43b1e39e048fd815a8b2896a1cf1
scosman <scosman@users.noreply.github.com>
2024-09-02 00:01:21 -0400

    Inline CSS for faster first page loads.

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,9 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    // allow up to 150kb of style to be inlined with the HTML
+    // Faster FCP (First Contentful Paint) by reducing the number of requests
+    inlineStyleThreshold: 150000,
   },
   preprocess: vitePreprocess(),
 }
