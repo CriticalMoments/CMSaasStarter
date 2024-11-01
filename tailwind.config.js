6c757c54c3889110627ce65f52b01d6169c2469b
scosman <scosman@users.noreply.github.com>
2024-11-01 17:39:43 -0400

    Use import style for Node 23+

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import typography from "@tailwindcss/typography"
+import daisyui from "daisyui"
+
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [typography, daisyui],
   daisyui: {
     themes: [
       {
