095677e8c5ead3142c0237b49a6fc215b4fa2ae9
scosman <scosman@users.noreply.github.com>
2025-03-04 20:23:47 -0500

    temporarily remove daisyui config, as part of upgrade https://daisyui.com/docs/upgrade/

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,25 +7,26 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [typography, daisyui],
-  daisyui: {
-    themes: [
-      {
-        saasstartertheme: {
-          primary: "#180042",
-          "primary-content": "#fefbf6",
-          secondary: "#c7b9f8",
-          neutral: "#180042",
-          "neutral-content": "#fefbf6",
-          accent: "#db2777",
-          "accent-content": "#180042",
-          "base-content": "#180042",
-          "base-100": "#fefbf6",
-          "base-200": "#faedd6",
-          success: "#37d399",
-          error: "#f77272",
-        },
-      },
-    ],
-  },
+  plugins: [typography],
+  // plugins: [typography, daisyui],
+  // daisyui: {
+  //   themes: [
+  //     {
+  //       saasstartertheme: {
+  //         primary: "#180042",
+  //         "primary-content": "#fefbf6",
+  //         secondary: "#c7b9f8",
+  //         neutral: "#180042",
+  //         "neutral-content": "#fefbf6",
+  //         accent: "#db2777",
+  //         "accent-content": "#180042",
+  //         "base-content": "#180042",
+  //         "base-100": "#fefbf6",
+  //         "base-200": "#faedd6",
+  //         success: "#37d399",
+  //         error: "#f77272",
+  //       },
+  //     },
+  //   ],
+  // },
 }
